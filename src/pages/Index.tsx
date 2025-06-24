8de18ea93c1340097313decabe6e90a90953b622
OpportunityWatch <rwcampbell2@gmail.com>
2025-06-24 00:40:20 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
-
 import React from 'react';
+import NightSkyBackground from '@/components/NightSkyBackground';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
@@ -11,23 +11,28 @@ import CTA from '@/components/CTA';
 import Footer from '@/components/Footer';
 
 const Index = () => {
-  // Update title
   React.useEffect(() => {
     document.title = "CometCopters - LED Slingshot Helicopters | Up to 150+ Feet High";
   }, []);
 
   return (
-    <div className="min-h-screen bg-comet-space">
-      <Navbar />
-      <Hero />
-      <Features />
-      <HowItWorks />
-      <Pricing />
-      <Testimonials />
-      <FAQ />
-      <CTA />
-      <Footer />
-    </div>
+    <>
+      {/* Animated Night Sky Background */}
+      <NightSkyBackground />
+      
+      {/* Content Overlay - positioned relative to appear above background */}
+      <div className="min-h-screen relative z-10">
+        <Navbar />
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <Pricing />
+        <Testimonials />
+        <FAQ />
+        <CTA />
+        <Footer />
+      </div>
+    </>
   );
 };
 
